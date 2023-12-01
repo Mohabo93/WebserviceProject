59a44789fb40cfeec71ade998dde751e3449b494
Mohabo93 <93mohabo@gafe.molndal.se>
2023-12-01 14:21:54 +0100

    done

--- src/main/java/com/example/WebserviceProject/WebserviceProjectApplication.java
+++ src/main/java/com/example/WebserviceProject/WebserviceProjectApplication.java
@@ -22,18 +22,25 @@ public class WebserviceProjectApplication {
 	}
 
 
+	// CommandLineRunner för att köra kod vid uppstart av programmet
 	@Bean
 	CommandLineRunner run(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncode){
 		return args ->{
-			if(roleRepository.findByAuthority("ADMIN").isPresent()) return;
+
+			if(roleRepository.findByAuthority("ADMIN").isPresent()) return;	// Kontrollera om ADMIN-rollen redan finns
+
+			// Skapa och spara ADMIN- och USER-rollerna
 			Role adminRole = roleRepository.save(new Role("ADMIN"));
 			roleRepository.save(new Role("USER"));
 
+			//Skapa en uppsättning roller med endast ADMIN-rollen
 			Set<Role> roles = new HashSet<>();
 			roles.add(adminRole);
 
+			// Skapa en Admin-användare med ID 1, användarnamn & lösenord och tilldelade roller
 			User admin = new User(1, "mohamad", passwordEncode.encode("123456"), roles);
 
+			// Sparar Admin-användaren i userRepository
 			userRepository.save(admin);
 		};
 	}
